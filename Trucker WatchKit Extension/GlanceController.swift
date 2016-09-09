af2f543dc9eaccd460db6221c4f197129e814a94
Kaspar Etter <kaspar.etter@digitalid.net>
2016-09-09 23:01:29 +0200

    Commit of watch template.

---
+++ Trucker WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Trucker WatchKit Extension
+//
+//  Created by Kaspar Etter on 9.9.2016.
+//  Copyright © 2016 Techfest Munich. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
